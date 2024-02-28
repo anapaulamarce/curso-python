27bb325b2276f146f167a2728037102eedd04e61
anapaulamarce <anapmarcelino98@gmail.com>
2024-02-28 11:59:06 -0300

    desafio 01

---
+++ desafio01.py
@@ -0,0 +1,8 @@
+## Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado 
+## Qual seu nome? Paula
+## Olá Paula! Prazer em te conhecer!## 
+
+nome = input('Qual seu nome? ')
+
+print(nome)
+print('Olá, ' + nome + '! Prazer em te conhecer!')
