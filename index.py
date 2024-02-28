e9e1ed47618e502812a7d9122474fd98e672e999
anapaulamarce <anapmarcelino98@gmail.com>
2024-02-28 11:45:50 -0300

    primeiro script

---
+++ index.py
@@ -0,0 +1,10 @@
+nome = 'Paula'
+idade = 25
+peso = 88
+
+
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos voce tem? ')
+peso = input('Quanto voce pesa? ')
+
+print(nome, idade, peso)
