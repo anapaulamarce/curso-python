1144692244aa0240bf1d03a84076061ee5b2c70f
anapaulamarce <anapmarcelino98@gmail.com>
2024-02-28 12:41:38 -0300

    Desafio 03

---
+++ desafio03.py
@@ -0,0 +1,10 @@
+## Crie um script Python que leia dois numeros e tente mostrar a soma entre eles
+## Primeiro numero: 
+## Segundo numero: 
+## A soma é 9
+
+num1 = input('Digite o primeiro número: ')
+num2  = input('Digite o primeiro número: ')
+soma = int(num1) + int(num2)
+
+print('A soma é ' + str(soma))
