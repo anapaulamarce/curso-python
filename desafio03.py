a3b69d5d9002f82a8667411cad5e3b0e45ebc533
anapaulamarce <anapmarcelino98@gmail.com>
2024-02-28 13:55:29 -0300

    Exercicio 1 - aula 06

--- desafio03.py
+++ desafio03.py
@@ -4,7 +4,7 @@
 ## A soma é 9
 
 num1 = input('Digite o primeiro número: ')
-num2  = input('Digite o primeiro número: ')
+num2 = input('Digite o primeiro número: ')
 soma = int(num1) + int(num2)
 
 print('A soma é ' + str(soma))
