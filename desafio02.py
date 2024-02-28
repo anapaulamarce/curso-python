908ed2f98550cc6254a14a74e758737443d384af
anapaulamarce <anapmarcelino98@gmail.com>
2024-02-28 12:07:02 -0300

    desafio 02

---
+++ desafio02.py
@@ -0,0 +1,11 @@
+## Crie um script Python que leia o dia, o mes e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+## Dia = 27
+## Mes = Jun
+## Ano = 1998
+## Você nasceu no dia 27 de Jun de 1998. Correto? 
+
+dia = input('Qual dia você nasceu? ')
+mes = input('Qual mês você nasceu? ')
+ano = input('Qual ano você nasceu? ')
+
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
