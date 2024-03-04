467381cbbf9502252ce580a3c7ee117ea674dd86
anapaulamarce <anapmarcelino98@gmail.com>
2024-03-04 01:24:07 -0300

    desafio 07 - aula 07

---
+++ desafio07.py
@@ -0,0 +1,7 @@
+## Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+num1 = int(input('Digite a primeira nota: '))
+num2 = int(input('Digite a segunda nota: '))
+media = (num1 + num2) / 2
+
+print('A média do aluno é ', media)
