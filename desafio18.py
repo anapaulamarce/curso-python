c974366591ae6754e1ae3bd951187b3693f041db
anapaulamarce <anapmarcelino98@gmail.com>
2024-03-11 11:40:01 -0300

    desafio 18 - aula 08

--- desafio18.py
+++ desafio18.py
@@ -1,2 +1,13 @@
 ## Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo
 
+from math import sin, cos, tan, radians
+
+ang = float(input('Digite o ângulo: '))
+sen = sin(radians(ang)) ## converter o grau para radiano
+cose = cos(radians(ang))
+tang = tan(radians(ang))
+
+print('Ângulo {}º'.format(ang))
+print('Seu seno é {:.2f}'.format(sen))
+print('Seu coseno é {:.2f}'.format(cose))
+print('Sua tangente é {:.2f}'.format(tang))
